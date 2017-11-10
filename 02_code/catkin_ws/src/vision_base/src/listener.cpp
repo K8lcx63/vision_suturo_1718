15db39ec5be4a81d225dce82ab62843c9dd6fe03
Unknown <ta_wu@uni-bremen.de>
2017-11-10 11:37:24 +0100

    Tutorial von robotica.unileon.es eingefuegt (statt planar_segmentation)

--- 02_code/catkin_ws/src/vision_base/src/listener.cpp
+++ 02_code/catkin_ws/src/vision_base/src/listener.cpp
@@ -10,26 +10,16 @@
 // includes for pcl
 
 #include <pcl/conversions.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/point_types.h>
-#include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-// Includes for planar-segmentation
-
-#include <iostream>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/voxel_grid.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-
-// Includes for cluster extraction
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 
 // #include <type_traits>
@@ -56,56 +46,74 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_out (new pcl::PointCloud<pcl::PointX
  ** Find an object-cluster (i.e. Box of passierte Tomaten)
 **/
 void findCluster(const sensor_msgs::PointCloud2 kinect_cloud) {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr plane_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
-
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PCLPointCloud2 pcl_cloud;
   pcl_conversions::toPCL(kinect_cloud, pcl_cloud);
-
   pcl::PointCloud<pcl::PointXYZ> *dummy_cloud(new pcl::PointCloud<pcl::PointXYZ>);
   pcl::fromPCLPointCloud2(pcl_cloud, *dummy_cloud);
+
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(dummy_cloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr convexHull(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr objects(new pcl::PointCloud<pcl::PointXYZ>);
 
-      std::cerr << "Point cloud data: " << cloud->points.size () << " points" << std::endl;
+  // Get the plane model, if present.
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  pcl::SACSegmentation<pcl::PointXYZ> segmentation;
+  segmentation.setInputCloud(cloud);
+  segmentation.setModelType(pcl::SACMODEL_PLANE);
+  segmentation.setMethodType(pcl::SAC_RANSAC);
+  segmentation.setDistanceThreshold(0.01);
+  segmentation.setOptimizeCoefficients(true);
+  pcl::PointIndices::Ptr planeIndices(new pcl::PointIndices);
+  segmentation.segment(*planeIndices, *coefficients);
 
+  if (planeIndices->indices.size() == 0)
+  	std::cout << "Could not find a plane in the scene." << std::endl;
+  else
+  {
+  	// Copy the points of the plane to a new cloud.
+  	pcl::ExtractIndices<pcl::PointXYZ> extract;
+  	extract.setInputCloud(cloud);
+  	extract.setIndices(planeIndices);
+  	extract.filter(*plane);
 
-      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-        // Create the segmentation object
-        pcl::SACSegmentation<pcl::PointXYZ> seg;
-        // Optional
-        seg.setOptimizeCoefficients (true);
-        // Mandatory
-        seg.setModelType (pcl::SACMODEL_PLANE);
-        seg.setMethodType (pcl::SAC_RANSAC);
-        seg.setDistanceThreshold (0.01);
+  	// Retrieve the convex hull.
+  	pcl::ConvexHull<pcl::PointXYZ> hull;
+  	hull.setInputCloud(plane);
+  	// Make sure that the resulting hull is bidimensional.
+  	hull.setDimension(2);
+  	hull.reconstruct(*convexHull);
 
-	int nr_points = (int) cloud->points.size();
-	while(cloud->points.size() > 0.05 * nr_points) // Während noch mindestens 10% der ursprünglichen Cloud erhalten sind...
-	{
-        seg.setInputCloud (cloud);
-        seg.segment (*inliers, *coefficients);
+  	// Redundant check.
+  	if (hull.getDimension() == 2)
+  	{
+  		// Prism object.
+  		pcl::ExtractPolygonalPrismData<pcl::PointXYZ> prism;
+  		prism.setInputCloud(cloud);
+  		prism.setInputPlanarHull(convexHull);
+  		// First parameter: minimum Z value. Set to 0, segments objects lying on the plane (can be negative).
+  		// Second parameter: maximum Z value, set to 10cm. Tune it according to the height of the objects you expect.
+  		prism.setHeightLimits(0.0f, 0.5f);
+  		pcl::PointIndices::Ptr objectIndices(new pcl::PointIndices);
 
-        if (inliers->indices.size () == 0)
-        {
-          PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-        }
-        // Extract the inliers
-            extract.setInputCloud(cloud);
-            extract.setIndices (inliers);
-            extract.setNegative (true);
-            extract.filter (*plane_cloud);
-            std::cerr << "PointCloud representing the planar component: " << plane_cloud->width * plane_cloud->height << " data points." << std::endl;
+  		prism.segment(*objectIndices);
 
-            // Create the filtering object
-            extract.setNegative (true);
-            extract.filter (*cloud_f);
-            cloud.swap (cloud_f);
-            //i++;
-	}
-            plane_out = plane_cloud;
+  		// Get and show all points retrieved by the hull.
+  		extract.setIndices(objectIndices);
+  		extract.filter(*objects);
+  		pcl::visualization::CloudViewer viewerObjects("Objects on table");
+  		viewerObjects.showCloud(objects);
+
+  		while (!viewerObjects.wasStopped())
+  		{
+  			// Do nothing but wait.
+  		}
+  	}
+  	else std::cout << "The chosen hull is not planar." << std::endl;
+  	}
+    // plane_out for advertising
+    plane_out = objects;
+
 }
 
 
@@ -136,7 +144,7 @@ int main(int argc, char **argv)
 	// Subscriber für das points-Topic des Kinect-Sensors.
 	ros::init(argc, argv, "listener");
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("/head_mount_kinect/depth_registered/points", 1, callback);
+	ros::Subscriber sub = n.subscribe("/head_mount_kinect/depth_registered/points", 1, &callback);
 	ros::Subscriber sub_kinect = n.subscribe("/head_mount_kinect/depth_registered/points", 1, &findCluster);
 
 	// ServiceClient für das Abrufen der Eistee-Position aus Gazebo.
