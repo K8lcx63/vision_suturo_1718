8c3c714f1bbbdd48c74f25aa1a03e03a151e4e93
Unknown <ta_wu@uni-bremen.de>
2017-12-09 15:14:52 +0100

    pose detection funktioniert. service angepasst

--- vision/src/globals.h
+++ vision/src/globals.h
@@ -6,13 +6,14 @@ ros::ServiceClient client;
 std::string
         error_message; // Wird durch den Object Position Service mit ausgegeben
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr kinect_global;
-pcl::PointCloud<pcl::PointXYZ>::Ptr objects_global;
-pcl::PointCloud<pcl::Normal>::Ptr normals_global;
-pcl::PointCloud<pcl::PointNormal>::Ptr pointnormals_global;
-pcl::PointCloud<pcl::PointXYZ>::Ptr mesh_global;
-pcl::PointXYZ object_pose_before;
-pcl::PointXYZ object_pose_after;
+pcl::PointCloud<pcl::PointXYZ>::Ptr kinect_global (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr objects_global (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::Normal>::Ptr normals_global (new pcl::PointCloud<pcl::Normal>);
+pcl::PointCloud<pcl::PointNormal>::Ptr pointnormals_global (new pcl::PointCloud<pcl::PointNormal>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr mesh_global (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr objects_rotated_global ( new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointXYZ object_pose_before ;
+pcl::PointXYZ object_pose_after ;
 
 
 geometry_msgs::PointStamped centroid_stamped;
