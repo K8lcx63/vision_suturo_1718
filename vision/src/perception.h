3579fe8d5344c01760e7fe7713213bc3c6916c48
Unknown <ta_wu@uni-bremen.de>
2017-12-17 23:12:59 +0100

    neue funktionen für pose_estimation (in dev). build funzt wieder

--- vision/src/perception.h
+++ vision/src/perception.h
@@ -4,13 +4,9 @@
 #include <Eigen/Geometry>
 #include <gazebo_msgs/GetModelState.h>
 #include <geometry_msgs/PointStamped.h>
-
-#include "saving.h"
 #include <pcl/ModelCoefficients.h>
 #include <pcl/PointIndices.h>
-#include <pcl/common/transforms.h>
 #include <pcl/conversions.h>
-#include <pcl/features/normal_3d.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/passthrough.h>
@@ -20,25 +16,47 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <ros/ros.h>
 #include <iostream>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/transformation_estimation.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/segmentation/extract_polygonal_prism_data.h>
 #include <pcl/filters/voxel_grid.h>
-#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/surface/mls.h>
+#include "FeatureCloud.h"
+#include "TemplateAlignment.h"
+#include "saving.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/correspondence.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/features/board.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/recognition/cg/hough_3d.h>
+#include <pcl/recognition/cg/geometric_consistency.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
 
+unsigned int input_noise_threshold = 342;
+bool simulation;
+
+/** Shorthand for Correspondence grouping **/
+typedef pcl::PointXYZ PointType;
+typedef pcl::Normal NormalType;
+typedef pcl::ReferenceFrame RFType;
+typedef pcl::SHOT352 DescriptorType;
+
+
+/** Shorthand **/
 typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloudXYZPtr;
 typedef pcl::PointCloud<pcl::Normal>::Ptr PointCloudNormalPtr;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
 typedef pcl::PointCloud<pcl::Normal> PointCloudNormal;
 typedef pcl::PointIndices::Ptr PointIndices;
 
-unsigned int input_noise_threshold = 342;
 
-bool simulation;
+/** Function Headers **/
 
 void findCluster(const PointCloudXYZPtr kinect);
 
@@ -67,16 +85,24 @@ void createCovarianceMatrix(PointCloudXYZ input,
 PointCloudXYZPtr apply3DFilter(PointCloudXYZPtr input, float x, float y,
                                float z);
 
-double executeICP(PointCloudXYZPtr cloud_in, PointCloudXYZPtr cloud_out);
+double executeICPwithIA();
 
 PointCloudXYZPtr mlsFilter(PointCloudXYZPtr input);
 
 PointCloudXYZPtr voxelGridFilter(PointCloudXYZPtr input);
 
+int computeCorrespondece();
+
+
+/** -------------------------- BEGIN OF IMPLEMENTATION ---------------------- **/
+
 /**
- ** Find the object!
-**/
+ * Find the object!
+ * @param kinect
+ */
 void findCluster(const PointCloudXYZPtr kinect) {
+    findCenterGazebo(); // check if simulation is ongoing
+
     PointCloudXYZPtr kinect1(new PointCloudXYZ), kinect2(new PointCloudXYZ);
     PointCloudXYZPtr cloud(new PointCloudXYZ); // for passthroughfilter
     PointCloudXYZPtr objects(new PointCloudXYZ);
@@ -141,9 +167,13 @@ void findCluster(const PointCloudXYZPtr kinect) {
     }
 }
 
+/**
+ * Returns a fake point (not estimated) with its origin in the model in simulation.
+ * It is only called, when findCluster() cannot find a point in simulation
+ * @return
+ */
 geometry_msgs::PointStamped
-findCenterGazebo() // Only called if something went wrong in findCluster()
-{
+findCenterGazebo() {
     if (simulation) // Check if this is a simulation. This function is useless if
         // it isn't :(
     {
@@ -162,6 +192,11 @@ findCenterGazebo() // Only called if something went wrong in findCluster()
     return centroid_stamped;
 }
 
+/**
+ * finding the geometrical center of a given pointcloud
+ * @param object_cloud
+ * @return
+ */
 geometry_msgs::PointStamped findCenter(const PointCloudXYZPtr object_cloud) {
     if (object_cloud->points.size() != 0) {
         int cloud_size = object_cloud->points.size();
@@ -186,6 +221,11 @@ geometry_msgs::PointStamped findCenter(const PointCloudXYZPtr object_cloud) {
     }
 }
 
+/**
+ * estimating surface normals
+ * @param input
+ * @return
+ */
 PointCloudNormalPtr estimateSurfaceNormals(PointCloudXYZPtr input) {
     ROS_INFO("ESTIMATING SURFACE NORMALS");
 
@@ -204,6 +244,12 @@ PointCloudNormalPtr estimateSurfaceNormals(PointCloudXYZPtr input) {
     return cloud_normals;
 }
 
+/**
+ * Creates a Pointnormal cloud from a normal cloud and a regular PointXYZ cloud
+ * @param input
+ * @param normals
+ * @return
+ */
 pcl::PointCloud<pcl::PointNormal>::Ptr
 createPointNormals(PointCloudXYZPtr input, PointCloudNormalPtr normals) {
     ROS_INFO("CREATE SURFACE POINTNORMALS");
@@ -211,40 +257,52 @@ createPointNormals(PointCloudXYZPtr input, PointCloudNormalPtr normals) {
             new pcl::PointCloud<pcl::PointNormal>);
     output->reserve(input->points.size());
     // TODO: Fix assignment of points
+    /*
     for (int i = 0; i < input->points.size(); i++) {
 
         pcl::PointXYZ point_xyz;
-        pcl::Normal normals;
+        pcl::Normal normals_point;
         pcl::PointNormal point_normal;
 
         // output->points[i].normal = normals->points[i].normal;
-        normals.normal_x = normals->points[i].normal_x;
-        normals.normal_y = normals->points[i].normal_y;
-        normals.normal_z = normals->points[i].normal_z;
+        normals_point.normal_x = normals_point->points[i].normal_x;
+        normals_point.normal_y = normals_point->points[i].normal_y;
+        normals_point.normal_z = normals_point->points[i].normal_z;
         // output->points[i].curvature = normals->points[i].curvature;
 
         point_xyz.x = input->points[i].x;
         point_xyz.y = input->points[i].y;
         point_xyz.z = input->points[i].z;
-        output->points.push_back(point_xyz, point_normal);
+        output->points.push_back(point_xyz, normals_point);
     }
+    */
     return output;
 }
 
+/**
+ * compute if a certain object is standing (mostly _global pointclouds are used)
+ * @return
+ */
 int objectIsStanding() {
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/tammo/.ros/eistee_mesh.pcd", *mesh_global) == -1) //* load the file
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/tammo/catkin_ws/src/vision_suturo_1718/vision/data/eistee_mesh.pcd",
+                                            *mesh_global) == -1) //* load the file
     {
         PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
     }
 
-    if (executeICP(objects_rotated_global, mesh_global) > 0.0006) {
-        return 0;
+    if (computeCorrespondece() >= 1) {
+        return 1;
     }
 
-    return 1;
+    return 0;
 }
 
+/**
+ * Rotates a pointcloud. parameters have to be manually set
+ * @param cloud
+ * @return
+ */
 PointCloudXYZPtr rotatePointCloud(PointCloudXYZPtr cloud) {
 
     float theta = M_PI; // the angle of rotation in radians
@@ -263,6 +321,15 @@ PointCloudXYZPtr rotatePointCloud(PointCloudXYZPtr cloud) {
     return transformed_cloud;
 }
 
+/**
+ * apply a passthrough Filter to all dimensions, reducing points and
+ * narrowing Field of Vision
+ * @param input
+ * @param x
+ * @param y
+ * @param z
+ * @return
+ */
 PointCloudXYZPtr apply3DFilter(PointCloudXYZPtr input, float x, float y,
                                float z) {
 
@@ -297,6 +364,11 @@ PointCloudXYZPtr apply3DFilter(PointCloudXYZPtr input, float x, float y,
     return input_after_xyz;
 }
 
+/**
+ * estimating plane indices
+ * @param input
+ * @return
+ */
 PointIndices estimatePlaneIndices(PointCloudXYZPtr input) {
     ROS_INFO("PLANE INDICES");
     PointIndices planeIndices(new pcl::PointIndices);
@@ -313,6 +385,12 @@ PointIndices estimatePlaneIndices(PointCloudXYZPtr input) {
     return planeIndices;
 }
 
+/**
+ * prism segmentation
+ * @param input_cloud
+ * @param plane
+ * @return
+ */
 PointIndices prismSegmentation(PointCloudXYZPtr input_cloud, PointCloudXYZPtr plane) {
     PointCloudXYZPtr plane_hull = plane;
     ROS_INFO("Starting prism segmentation...");
@@ -330,6 +408,13 @@ PointIndices prismSegmentation(PointCloudXYZPtr input_cloud, PointCloudXYZPtr pl
     return prism_indices;
 }
 
+/**
+ * extract a pointcloud by indices from an input pointcloud
+ * @param input
+ * @param indices
+ * @param negative
+ * @return
+ */
 PointCloudXYZPtr extractCluster(PointCloudXYZPtr input, PointIndices indices, bool negative) {
     ROS_INFO("CLUSTER EXTRACTION");
     PointCloudXYZPtr objects(new PointCloudXYZ);
@@ -341,10 +426,55 @@ PointCloudXYZPtr extractCluster(PointCloudXYZPtr input, PointIndices indices, bo
     return objects;
 }
 
-double executeICP(PointCloudXYZPtr cloud_in, PointCloudXYZPtr cloud_out) {
+/**
+ * compute iterative closest point with initial alignment beforehand
+ * @return
+ */
+double executeICPwithIA() {
+
+    // TODO Fix with multiple input files (pcl documentation tutorial)
+
+
+    // load FeatureClouds
+    FeatureCloud template_cloud;
+    template_cloud.loadInputCloud("/home/tammo/catkin_ws/src/vision_suturo_1718/vision/data/eistee_mesh.pcd");
+
+    FeatureCloud target_cloud;
+    target_cloud.setInputCloud(kinect_global);
+
+    // set template clouds
+    TemplateAlignment template_align;
+
+    template_align.addTemplateCloud(template_cloud);
+    template_align.setTargetCloud(target_cloud);
+
+    // Find the best template alignment
+    TemplateAlignment::Result best_alignment;
+
+    // Print the rotation matrix and translation vector
+    Eigen::Matrix3f rotation = best_alignment.final_transformation.block<3, 3>(0, 0);
+    Eigen::Vector3f translation = best_alignment.final_transformation.block<3, 1>(0, 3);
+
+    printf("\n");
+    printf("    | %6.3f %6.3f %6.3f | \n", rotation(0, 0), rotation(0, 1), rotation(0, 2));
+    printf("R = | %6.3f %6.3f %6.3f | \n", rotation(1, 0), rotation(1, 1), rotation(1, 2));
+    printf("    | %6.3f %6.3f %6.3f | \n", rotation(2, 0), rotation(2, 1), rotation(2, 2));
+    printf("\n");
+    printf("t = < %0.3f, %0.3f, %0.3f >\n", translation(0), translation(1), translation(2));
+
+    // Save the aligned template for visualization
+    pcl::PointCloud<pcl::PointXYZ> transformed_cloud;
+    pcl::PointCloud<pcl::PointXYZ> tmpl_for_trans = *kinect_global; //template_cloud.getPointCloud();
+    pcl::transformPointCloud(tmpl_for_trans, transformed_cloud, best_alignment.final_transformation);
+    pcl::io::savePCDFileBinary("output.pcd", transformed_cloud);
+
+    // succeedingly do the icp transformation
+
+    PointCloudXYZPtr transformed_cloud_ptr(new PointCloudXYZ);
+    *transformed_cloud_ptr = transformed_cloud;
     pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-    icp.setInputSource(cloud_in);
-    icp.setInputTarget(cloud_out);
+    icp.setInputSource(transformed_cloud_ptr);
+    icp.setInputTarget(mesh_global);
     //icp.setMaximumIterations(10);
     pcl::PointCloud<pcl::PointXYZ> final;
     icp.align(final);
@@ -354,8 +484,12 @@ double executeICP(PointCloudXYZPtr cloud_in, PointCloudXYZPtr cloud_out) {
     return icp.getFitnessScore();
 }
 
-//TODO inspect where it fails here
+/**
+ * rotate on point cloud to the orientation of the other
+ */
 void rotateCloud2Cloud() {
+    //TODO inspect where it fails here
+
     Eigen::Vector4f centroid_objects;
     Eigen::Vector4f centroid_mesh;
 
@@ -394,6 +528,11 @@ void rotateCloud2Cloud() {
     //pcl::transformPointCloud(*objects_global,*mesh_global,rot_mat);
 }
 
+/**
+ * Filtering the input cloud with a moving least squares algorithm
+ * @param input
+ * @return
+ */
 PointCloudXYZPtr mlsFilter(PointCloudXYZPtr input) {
     PointCloudXYZPtr result(new PointCloudXYZ);
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
@@ -418,6 +557,12 @@ PointCloudXYZPtr mlsFilter(PointCloudXYZPtr input) {
     return result;
 }
 
+
+/**
+ * Filtering the input cloud with a voxel grid
+ * @param input
+ * @return
+ */
 PointCloudXYZPtr voxelGridFilter(PointCloudXYZPtr input) {
     PointCloudXYZPtr result(new PointCloudXYZ);
 
@@ -426,6 +571,145 @@ PointCloudXYZPtr voxelGridFilter(PointCloudXYZPtr input) {
     sor.setLeafSize(0.01f, 0.01f, 0.01f);
     sor.filter(*result);
     return result;
+}
+
+/** correspondence grouping from pcl **/
+int computeCorrespondece() {
+
+    bool show_keypoints_(false);
+    bool show_correspondences_(false);
+    bool use_cloud_resolution_(false);
+    bool use_hough_(true);
+    float model_ss_(0.01f);
+    float scene_ss_(0.03f);
+    float rf_rad_(0.015f);
+    float descr_rad_(0.02f);
+    float cg_size_(0.01f);
+    float cg_thresh_(5.0f);
+
+    pcl::PointCloud<PointType>::Ptr model(new pcl::PointCloud<PointType>());
+    pcl::PointCloud<PointType>::Ptr model_keypoints(new pcl::PointCloud<PointType>());
+    pcl::PointCloud<PointType>::Ptr scene_keypoints(new pcl::PointCloud<PointType>());
+    pcl::PointCloud<NormalType>::Ptr model_normals(new pcl::PointCloud<NormalType>());
+    pcl::PointCloud<NormalType>::Ptr scene_normals(new pcl::PointCloud<NormalType>());
+    pcl::PointCloud<DescriptorType>::Ptr model_descriptors(new pcl::PointCloud<DescriptorType>());
+    pcl::PointCloud<DescriptorType>::Ptr scene_descriptors(new pcl::PointCloud<DescriptorType>());
+
+    //
+    //  Compute Normals
+    //
+    pcl::NormalEstimationOMP<PointType, NormalType> norm_est;
+    norm_est.setKSearch(10);
+    norm_est.setInputCloud(mesh_global);
+    norm_est.compute(*model_normals);
+
+    std::cout << "Size of mesh normals " << model_normals->size() << std::endl;
+    norm_est.setInputCloud(kinect_global);
+    norm_est.compute(*scene_normals);
+
+
+    pcl::UniformSampling<PointType> uniform_sampling;
+    uniform_sampling.setInputCloud(mesh_global);
+    uniform_sampling.setRadiusSearch(0.01f);
+    uniform_sampling.filter(*model_keypoints);
+    std::cout << "Model total points: " << model->size() << "; Selected Keypoints: " << model_keypoints->size()
+              << std::endl;
+
+    uniform_sampling.setInputCloud(kinect_global);
+    uniform_sampling.setRadiusSearch(scene_ss_);
+    uniform_sampling.filter(*scene_keypoints);
+    std::cout << "Scene total points: " << kinect_global->size() << "; Selected Keypoints: " << scene_keypoints->size()
+              << std::endl;
+
+
+    //
+    //  Compute Descriptor for keypoints
+    //
+    pcl::SHOTEstimationOMP<PointType, NormalType, DescriptorType> descr_est;
+    descr_est.setRadiusSearch(descr_rad_);
+
+    descr_est.setInputCloud(model_keypoints);
+    descr_est.setInputNormals(model_normals);
+    descr_est.setSearchSurface(mesh_global);
+    descr_est.compute(*model_descriptors);
+
+    descr_est.setInputCloud(scene_keypoints);
+    descr_est.setInputNormals(scene_normals);
+    descr_est.setSearchSurface(kinect_global);
+    descr_est.compute(*scene_descriptors);
+
+    //
+    //  Find Model-Scene Correspondences with KdTree
+    //
+    pcl::CorrespondencesPtr model_scene_corrs(new pcl::Correspondences());
+
+    pcl::KdTreeFLANN<DescriptorType> match_search;
+    match_search.setInputCloud(model_descriptors);
+
+    //  For each scene keypoint descriptor, find nearest neighbor into the model keypoints descriptor cloud and add it to the correspondences vector.
+    for (size_t i = 0; i < scene_descriptors->size(); ++i) {
+        std::vector<int> neigh_indices(1);
+        std::vector<float> neigh_sqr_dists(1);
+        if (!pcl_isfinite (scene_descriptors->at(i).descriptor[0])) //skipping NaNs
+        {
+            continue;
+        }
+        int found_neighs = match_search.nearestKSearch(scene_descriptors->at(i), 1, neigh_indices, neigh_sqr_dists);
+        if (found_neighs == 1 && neigh_sqr_dists[0] <
+                                 0.25f) //  add match only if the squared descriptor distance is less than 0.25 (SHOT descriptor distances are between 0 and 1 by design)
+        {
+            pcl::Correspondence corr(neigh_indices[0], static_cast<int> (i), neigh_sqr_dists[0]);
+            model_scene_corrs->push_back(corr);
+        }
+    }
+    std::cout << "Correspondences found: " << model_scene_corrs->size() << std::endl;
+
+    //
+    //  Actual Clustering
+    //
+    std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > rototranslations;
+    std::vector<pcl::Correspondences> clustered_corrs;
+
+    //  Using Hough3D
+
+    //
+    //  Compute (Keypoints) Reference Frames only for Hough
+    //
+    pcl::PointCloud<RFType>::Ptr model_rf(new pcl::PointCloud<RFType>());
+    pcl::PointCloud<RFType>::Ptr scene_rf(new pcl::PointCloud<RFType>());
+
+    pcl::BOARDLocalReferenceFrameEstimation<PointType, NormalType, RFType> rf_est;
+    rf_est.setFindHoles(true);
+    rf_est.setRadiusSearch(rf_rad_);
+
+    rf_est.setInputCloud(model_keypoints);
+    rf_est.setInputNormals(model_normals);
+    rf_est.setSearchSurface(mesh_global);
+    rf_est.compute(*model_rf);
+
+    rf_est.setInputCloud(scene_keypoints);
+    rf_est.setInputNormals(scene_normals);
+    rf_est.setSearchSurface(kinect_global);
+    rf_est.compute(*scene_rf);
+
+    //  Clustering
+    pcl::Hough3DGrouping<PointType, PointType, RFType, RFType> clusterer;
+    clusterer.setHoughBinSize(cg_size_);
+    clusterer.setHoughThreshold(cg_thresh_);
+    clusterer.setUseInterpolation(true);
+    clusterer.setUseDistanceWeight(false);
+
+    clusterer.setInputCloud(model_keypoints);
+    clusterer.setInputRf(model_rf);
+    clusterer.setSceneCloud(scene_keypoints);
+    clusterer.setSceneRf(scene_rf);
+    clusterer.setModelSceneCorrespondences(model_scene_corrs);
+
+    //clusterer.cluster (clustered_corrs);
+    clusterer.recognize(rototranslations, clustered_corrs);
+
+    return rototranslations.size();
+
 }
 
 #endif // VISION_PERCEPTION_H
