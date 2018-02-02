c6da72f5c9ad9716af096cd338e1adce69417fcc
Unknown <link@uni-bremen.de>
2018-02-02 15:59:23 +0100

    Debug-Kram, neues Todo für mich, Pointclouds im Konstruktor initialisiert + externs

--- vision/src/perception/perception.h
+++ vision/src/perception/perception.h
@@ -20,6 +20,7 @@
 #include <pcl/filters/statistical_outlier_removal.h>
 #include "short_types.h"
 #include "transformer/CloudTransformer.h"
+#include "../saving/saving.h"
 #include <pcl/features/cvfh.h>
 #include <iterator>
 #include <vector>
@@ -50,7 +51,14 @@ PointCloudRGBPtr                SACInitialAlignment(std::vector<PointCloudRGBPtr
                                                     PointCloudRGBPtr target);
 PointCloudRGBPtr                iterativeClosestPoint(PointCloudRGBPtr input, PointCloudRGBPtr target);
 
-
-extern PointCloudRGBPtr cloud_final;
+extern PointCloudRGBPtr cloud_plane,
+        cloud_cluster,
+        cloud_cluster2,
+        cloud_f,
+        cloud_3df,
+        cloud_voxelgridf,
+        cloud_mlsf,
+        cloud_prism,
+        cloud_final;
 
 #endif //VISION_PERCEPTION_H
